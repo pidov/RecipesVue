3db5735a497214fb7598848098a889ba77f6f264
Ivan Pidov <ivan.pidov@tick42.com>
2017-04-25 15:52:31 +0300

    Cleanup

--- functions.php
+++ functions.php
@@ -105,15 +105,7 @@ add_action( 'widgets_init', 'recipes_widgets_init' );
  * Enqueue scripts and styles.
  */
 function recipes_scripts() {
-	wp_enqueue_style( 'recipes-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'recipes-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'recipes-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'recipes_scripts' );
 
