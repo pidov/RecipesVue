ea8bfb6314b78f774c2c53137143c6adc96ea725
Ivan Pidov <ivan.pidov@gmail.com>
2017-05-08 19:05:02 +0300

    Fixing assets path

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: 'wp-content/themes/recipes/static/',
+    assetsSubDirectory: 'wp-content/themes/recipe/dist/static',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
